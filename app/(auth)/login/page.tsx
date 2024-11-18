dc4f5f73233dd3ba72d81326dba38f4196b623da
maher <mansourmaher77@gmail.com>
2024-11-18 23:29:47 +0100

    Refactor Login component: remove Suspense wrapper and unused searchParams, streamline error handling

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -12,8 +12,6 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useState, useTransition } from "react";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 import {
   Form,
@@ -28,12 +26,6 @@ import { FormError } from "./_compoenets/Form-error";
 import { FormSucces } from "./_compoenets/Form-succes";
 
 export default function Login() {
-  const searchParams = useSearchParams();
-  const errorParam =
-    searchParams.get("error") === "OAuthAccountNotLinked"
-      ? "Account not linked with this email"
-      : "";
-
   const [isPending, startTransition] = useTransition();
   const [error, setError] = useState<string | undefined>("");
   const [succes, setSucces] = useState<string | undefined>("");
@@ -60,94 +52,92 @@ export default function Login() {
     });
   };
   return (
-    <Suspense>
-      <SlimLayout>
-        <div className="flex ">
-          <Link href="/" aria-label="Home">
-            <Logo className="h-10 w-auto" />
-          </Link>
-        </div>
-        <h2 className="mt-20 text-lg font-semibold text-gray-900">
-          Sign in to your account
-        </h2>
-        <p className="mt-2 text-sm text-gray-700">
-          Don’t have an account?{" "}
-          <Link
-            href="/register"
-            className="font-medium text-blue-600 hover:underline"
-          >
-            Sign up
-          </Link>{" "}
-          for a free trial.
-        </p>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="mt-10 grid grid-cols-1 gap-y-8"
-            aria-disabled={isPending}
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="email">Email address</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      required
-                      type="email"
-                      autoComplete="email"
-                      placeholder="Email address"
-                    />
-                  </FormControl>
-                  <FormMessage>
-                    {form.formState.errors.email?.message}
-                  </FormMessage>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      required
-                      type="password"
-                      placeholder="Password"
-                    />
-                  </FormControl>
-                  <FormMessage>
-                    {form.formState.errors.password?.message}
-                  </FormMessage>
-                </FormItem>
-              )}
-            />
-            <FormError message={error || errorParam} />
-            <FormSucces message={succes} />
-            <div>
-              <Button
-                type="submit"
-                variant="solid"
-                color="blue"
-                className="w-full"
-                disabled={isPending}
-                onClick={() => form.handleSubmit(onSubmit)}
-              >
-                <span>
-                  Sign in <span aria-hidden="true">&rarr;</span>
-                </span>
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </SlimLayout>
-    </Suspense>
+    <SlimLayout>
+      <div className="flex ">
+        <Link href="/" aria-label="Home">
+          <Logo className="h-10 w-auto" />
+        </Link>
+      </div>
+      <h2 className="mt-20 text-lg font-semibold text-gray-900">
+        Sign in to your account
+      </h2>
+      <p className="mt-2 text-sm text-gray-700">
+        Don’t have an account?{" "}
+        <Link
+          href="/register"
+          className="font-medium text-blue-600 hover:underline"
+        >
+          Sign up
+        </Link>{" "}
+        for a free trial.
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-10 grid grid-cols-1 gap-y-8"
+          aria-disabled={isPending}
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="email">Email address</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    required
+                    type="email"
+                    autoComplete="email"
+                    placeholder="Email address"
+                  />
+                </FormControl>
+                <FormMessage>
+                  {form.formState.errors.email?.message}
+                </FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    required
+                    type="password"
+                    placeholder="Password"
+                  />
+                </FormControl>
+                <FormMessage>
+                  {form.formState.errors.password?.message}
+                </FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormError message={error } />
+          <FormSucces message={succes} />
+          <div>
+            <Button
+              type="submit"
+              variant="solid"
+              color="blue"
+              className="w-full"
+              disabled={isPending}
+              onClick={() => form.handleSubmit(onSubmit)}
+            >
+              <span>
+                Sign in <span aria-hidden="true">&rarr;</span>
+              </span>
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </SlimLayout>
   );
 }
